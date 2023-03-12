be31cbadefdc09c1380f1e7d8b887a3e23cfafc7
TatianaSteshakova <65027776+TatianaSteshakova@users.noreply.github.com>
2023-03-12 22:14:49 +0300

    Add files via upload

---
+++ Homework4.4.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "Rus");
+
+    int n = 0;
+
+    cout << "Введите количество элементов в массиве: " << endl;
+    cin >> n;
+
+    int* mas = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Введите число mas[" << i + 1 << "]: " << endl;
+        cin >> mas[i];
+    }
+
+    int min = mas[0], max = mas[0], sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (mas[i] < min)
+        {
+            min = mas[i];
+        }
+        else if (mas[i] > max)
+        {
+            max = mas[i];
+        }
+        sum += mas[i];
+    }
+    double avg = (double) sum / n;
+
+    cout << "Минимальное число: " << min << endl;
+    cout << "Максимальное число: " << max << endl;
+    cout << "Среднее значение: " << avg << endl;
+}
